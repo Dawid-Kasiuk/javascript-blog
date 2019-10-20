2d24efdb7082df4552485e065ed60946803faa2c
Dawid Kasiuk <dawid_kasiuk@wp.pl>
2019-10-20 13:31:33 +0200

    Add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@
   });*/
 
   const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
+    const clickedElement = this;
+    /*console.log('Link was clicked!');
+    console.log(event);*/
+    console.log('clickedElement (with plus): ' + clickedElement);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,9 +19,10 @@
       activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.post');
 
